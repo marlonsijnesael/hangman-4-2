9f0f4b8f2e43fa3c412ec9d2d5a776c3ae601ee1
marlonsijnesael <fakiefsflip@gmail.com>
2018-06-10 14:48:21 +0200

    added

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Photon;
 
-public class TurnManager : PunBehaviour
-{
+public class TurnManager : PunBehaviour {
     [SerializeField]
-    private Player player1, player2;
-    public static Player currentPlayer;
-    public static Player waitingPlayer;
+    public Player[] players = new Player[2];
+    public static string currentPlayer;
+    public static string waitingPlayer;
+    public static GameObject CurrentplayerObject;
 
     public GameObject[] noosePlayer1, noosePlayer2;
     public GameObject noose1, noose2;
@@ -23,79 +23,75 @@ public class TurnManager : PunBehaviour
     public int player1Score, player2Score;
 
 
-    private void Awake()
-    {
-        currentPlayer = player1;
-        waitingPlayer = player2;
+    private void Start() {
+
+        for (int i = 0; i < PhotonNetwork.playerList.Length; i++) {
+            PhotonPlayer newpPlayer = PhotonNetwork.playerList[i];
+            players[i] = new Player(newpPlayer.ID, newpPlayer.NickName, 0, 0);
+        }
+
+        currentPlayer = players[0].nickName;
+        waitingPlayer = players[1].nickName;
+
         GetNooseComponents(noose1, noosePlayer1);
         GetNooseComponents(noose2, noosePlayer2);
         UpdateUI();
     }
 
     //puts the components of the gallow in an array
-    private void GetNooseComponents(GameObject noose, GameObject[] nooseArray)
-    {
+    private void GetNooseComponents(GameObject noose, GameObject[] nooseArray) {
         // noose.transform.childCount 
-        for (int i = 0; i < noose.transform.childCount; i++)
-        {
+        for (int i = 0; i < noose.transform.childCount; i++) {
             nooseArray[i] = noose.transform.GetChild(i).gameObject;
         }
     }
 
-    private void Update()
-    {
+    private void Update() {
         UpdateUI();
 
-        if (player1.wrongAnswers >= maxWrongAnswers || player2.wrongAnswers >= maxWrongAnswers)
-        {
+        if (players[0].wrongAnswers >= maxWrongAnswers || players[1].wrongAnswers >= maxWrongAnswers) {
             GameOver();
         }
     }
 
     //used in textfield to call the setturn on end edit
-    public void SetTurnOnEdit()
-    {
-        SetTurn(currentPlayer, waitingPlayer);
+    public void SetTurnOnEdit() {
+        photonView.RPC("SetTurn", PhotonTargets.AllBufferedViaServer, currentPlayer, waitingPlayer);
     }
 
-    private void SetTurn(Player _current , Player _waiting)
-    {
+    [PunRPC]
+    private void SetTurn(string _current, string _waiting) {
         turn++;
-       currentPlayer = _waiting;
-       waitingPlayer = _current;
+        currentPlayer = _waiting;
+        waitingPlayer = _current;
+      
     }
 
     //updates turn and score UI when called
-    private void UpdateUI()
-    {
+    private void UpdateUI() {
         turnUI.text = "Turn: " + turn.ToString();
-        //activeScoreUI.text = "Player 1: " + currentPlayer.nickName + " Score: " + currentPlayer.score;
-        //waitingScoreUI.text = "Player 2: " + waitingPlayer.nickName + " Score: " + waitingPlayer.score;
     }
 
-
     //game over screen
-    void GameOver()
-    {
+    void GameOver() {
         SceneManager.LoadScene(2);
     }
 
-    public void UpdateNoose()
-    {
+    public void UpdateNoose() {
 
-        int wronganswers = currentPlayer.wrongAnswers;
 
-        if (wronganswers > 0)
-        {
-            if (currentPlayer == player1)
-            {
-                noosePlayer1[player1.wrongAnswers -1].SetActive(true);
+
+        if (currentPlayer == players[0].nickName) {
+            if (players[0].wrongAnswers > 0) {
+                noosePlayer1[players[0].wrongAnswers - 1].SetActive(true);
             }
+        }
 
-            if (currentPlayer == player2)
-            {
-                noosePlayer2[player2.wrongAnswers-1].SetActive(true);
+        if (currentPlayer == players[1].nickName) {
+            if (players[1].wrongAnswers > 0) {
+                noosePlayer1[players[1].wrongAnswers - 1].SetActive(true);
             }
         }
     }
 }
+
